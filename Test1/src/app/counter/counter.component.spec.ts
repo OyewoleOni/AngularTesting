30b7e86694a65ec639cb80b32b36922114086768
Oni Oyewole <ooni@eminenttechnology.com>
2018-09-14 19:27:24 +0100

    Initial

---
+++ Test1/src/app/counter/counter.component.spec.ts
@@ -0,0 +1,89 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import{DebugElement} from '@angular/core'
+import{By} from '@angular/platform-browser'
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+  let debugElement : DebugElement;
+  let htmlElement : HTMLElement;
+  beforeEach(async(()=>{
+    TestBed.configureTestingModule({
+      declarations:[CounterComponent]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(CounterComponent);
+    component=fixture.componentInstance;
+    fixture.detectChanges();
+
+    debugElement = fixture.debugElement.query(By.css('p'));
+    htmlElement=debugElement.nativeElement;
+  });
+
+  it('should increment the counter number by 1',()=>{
+    //Arrange
+    const initialValue = component.counter;
+
+    //Act
+    component.increment();
+    fixture.detectChanges();
+
+    const newValue = component.counter;
+
+    //Assert
+    expect(newValue).toBeGreaterThan(initialValue);
+  })
+
+  it('should decrement the counter number by 1',()=>{
+    //Arrange
+    const initialValue = component.counter;
+
+    //Act
+    component.decrement();
+    fixture.detectChanges();
+    
+    const newValue = component.counter;
+
+    //Assert
+    expect(newValue).toBeLessThan(initialValue);
+  })
+
+  it('should display the counter number of the counter', ()=>{
+    //Assert that the text onScreen is of Number: 1
+    expect(htmlElement.textContent).toEqual('Number: 1');
+  })
+
+  it('should display the counter number on screen, after being incremented by one',()=>{
+    //Arrange
+
+    //Act
+    component.increment();
+    fixture.detectChanges()
+    
+
+    //Assert
+    expect(htmlElement.textContent).toEqual('Number: 2');
+  })
+
+
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ CounterComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(CounterComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
